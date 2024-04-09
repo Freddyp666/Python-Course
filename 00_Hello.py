d09898287f7c3507fbbe33e4a3972e104415130c
Freddy <Freddy@FREDDY>
2024-04-08 20:43:06 -0500

    #Variable

---
+++ 00_Hello.py
@@ -0,0 +1,19 @@
+"""
+Este es un comentario de inicio a fin
+"""
+print("hola, mundo")
+print('hola, mundo')
+
+
+#consultar el tipo de dato
+print(type(3+1)) #entero
+print(type(3-1.1)) #flotante
+print(type("hola mundo")) #cadena de texto
+print(type('hola mundo')) #cadena de texto
+print(type(True)) #booleano\
+print(type(1+3j)) #complejo
+print(type([1,2,3])) #lista
+print(type((1,2,3))) #tupla
+print(type({1,2,3})) #conjunto
+print(type({"nombre":"Juan"})) #diccionario
+print(type(None)) #NoneType
